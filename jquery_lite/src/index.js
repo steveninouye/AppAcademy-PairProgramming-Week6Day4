97cee462c3f32e6262e965c72e46fdb0856e3248
Steven Inouye <steveninouye@msn.com>
2018-11-29 14:03:00 -0800

    Start jQuery project

---
+++ jquery_lite/src/index.js
@@ -0,0 +1,10 @@
+window.$l = function(arg){
+  if(arg instanceof String){
+    if(arg[0] === "#"){
+      // it is an ID
+    }else if(arg[0]){
+      // it is an calss
+    }
+  }
+}
+// $(".small-div")
